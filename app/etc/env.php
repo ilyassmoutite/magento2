539b334a2c4b3e050563adfcd62f4ac31e7b5233
ilyassmoutite <45095556+ilyassmoutite@users.noreply.github.com>
2021-06-09 13:12:19 +0100

    Update env.php

--- app/etc/env.php
+++ app/etc/env.php
@@ -1,1 +1,86 @@
 
+<?php
+return [
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'backend' => [
+        'frontName' => 'admin_10s5xt'
+    ],
+    'crypt' => [
+        'key' => '026ad0b943814d1677321c26d6ca55d5'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1',
+                'dbname' => 'build_db',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'ccf_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'ccf_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+
+    ],
+    'install' => [
+        'date' => 'Wed, 09 Jun 2021 12:02:05 +0000'
+    ]
+];
